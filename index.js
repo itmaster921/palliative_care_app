32bcbfb088b9810bb1380662d3ff3190c10e7c24
itmaster921 <dev.mobile.99@hotmail.com>
2019-03-25 17:07:51 +0800

    keyboard manager

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import App from './src/App';
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-console.disableYellowBox = true
+
 
 AppRegistry.registerComponent('palliative_care_app', () => App);
--- src/App.js
+++ src/App.js
@@ -17,22 +17,6 @@ import {PrimaryNav} from "./Router";
 import KeyboardManager from 'react-native-keyboard-manager'
 import store from './Store';
 
-if(Platform.OS === 'ios')
-{
-  KeyboardManager.setEnable(true);
-  KeyboardManager.setEnableDebugging(true);
-  KeyboardManager.setKeyboardDistanceFromTextField(10);
-  KeyboardManager.setPreventShowingBottomBlankSpace(true);
-  KeyboardManager.setEnableAutoToolbar(true);
-  KeyboardManager.setToolbarDoneBarButtonItemText("Done");
-  KeyboardManager.setToolbarManageBehaviour(0);
-  KeyboardManager.setShouldToolbarUsesTextFieldTintColor(false);
-  KeyboardManager.setToolbarPreviousNextButtonEnable(false);
-  KeyboardManager.setShouldShowTextFieldPlaceholder(true);
-  KeyboardManager.setOverrideKeyboardAppearance(false);
-  KeyboardManager.setShouldResignOnTouchOutside(true);
-  KeyboardManager.resignFirstResponder();
-}
 
 class App extends Component {
 
